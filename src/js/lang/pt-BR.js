43cb5286d1e93e1463ad4bb28437425c14d51227
Dnn <contato@seusite.me>
2016-01-27 19:55:05 -0200

    Create pt-BR.js
    Language file for: Português Brasileiro (or Portuguese Brazilian)

---
+++ src/js/lang/pt-BR.js
@@ -0,0 +1,68 @@
+export default {
+    common: {
+        'ok': 'OK',
+        'exit': 'Sair',
+        'cancel': 'Cancelar',
+        'success': 'Sucesso',
+        'warning': 'Atenção',
+
+        'settings': 'Configurações',
+
+        'name': 'Nome',
+        'search': 'Procurar',
+
+        'text': 'Texto',
+        'all_files': 'Todos os Arquivos',
+
+        'update_complete': 'Atualização Completa',
+        'no_update_found': 'Nenhuma Atualização Encontrada',
+        'update_available': 'Atualização Disponível',
+        'found_new_version': 'Nova Versão Encontrada: $$1',
+        'using_latest_release': 'Você está usando a última versão.',
+        'confirm_update': 'Você quer atualizar para a última versão?',
+        'restart_to_update': 'Por favor, reinicie o $$1 para essa atualização ter efeito.',
+
+        'update_failed': 'Falha na atualização',
+        'check_update_failed': 'A verificação de atualização falhou.',
+        'apply_update_failed': 'Falha ao aplicar a atualização.',
+        'download_update_failed': 'O download da atualização falhou.',
+        'cannot_find_core_package': 'Não foi possível encontrar o pacote principal.',
+        'go_to_homepage': 'Por favor, vá até $$1 para fazer o download da última versão.',
+    },
+    main: {
+        'grant_permission': 'GARANTIR PERMISSÃO',
+        'dont_have_permission': 'Você não tem permissão para gravar no arquivo de hosts.',
+
+        'edit_hosts': 'Editar Hosts',
+        'create_new_hosts': 'Criar Novo Hosts',
+        'remote_source_url': 'URL da Origem Remota (Opcional)',
+
+        'hosts_rules': '$$1 Regras',
+    },
+    settings: {
+        'import': 'Importar',
+        'import_error': 'Erro ao Importar',
+        'import_from_zip': 'Importar Zip',
+        'import_complete': 'Importação completa',
+        'confirm_continue': 'Você quer continuar?',
+        'confirm_a_valid_file': 'Você abriu um arquivo válido?',
+        'overridden_warning': 'Seus arquivos atuais serão substituídos.',
+
+        'export': 'Exportar',
+        'export_to_zip': 'Exportar para Zip',
+        'export_to_surge': 'Exportar para Surge',
+
+        'language': 'Língua',
+        'language_changed': 'Linguagem Alterada',
+
+        'about': 'Sobre',
+        'homepage': 'Homepage',
+        'check_update': 'Verificar Atualização',
+        'checking_update': 'Verificando Atualização',
+        'applying_update': 'Aplicando Atualização',
+        'current_version': 'Versão Atual: $$1',
+        'downloading_update': 'Baixando Atualização',
+
+        'please_restart_app': 'Por favor, reinicie o $$1 para esta alteração ter efeito.',
+    },
+}
